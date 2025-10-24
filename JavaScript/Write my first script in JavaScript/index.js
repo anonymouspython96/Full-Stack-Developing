b709f72649c48068a65a9e5c53f878d1aec8a858
anonymouspython96 <emilianmail96@gmail.com>
2025-10-24 11:35:57 +0200

    Started JavaScript and variables.

---
+++ JavaScript/Write my first script in JavaScript/index.js
@@ -0,0 +1,13 @@
+let myAge = 29
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoints += 50
+console.log(bonusPoints)
+bonusPoints -= 75
+console.log(bonusPoints)
+bonusPoints += 45
+console.log(bonusPoints)
