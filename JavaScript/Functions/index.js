caf78a5c9ecb7e4d4c6e770019b77c6efa3a20a8
anonymouspython96 <emilianmail96@gmail.com>
2025-10-24 14:39:22 +0200

    writed a function that increments by one

---
+++ JavaScript/Functions/index.js
@@ -0,0 +1,18 @@
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function sumOfThreeNumbers() {
+    console.log(lap1 + lap2 + lap3)
+}
+
+sumOfThreeNumbers()
+
+// write a function that increments
+
+let lapsCompleted = 0
+function incrementOfLaps() {
+    lapsCompleted += 1
+}
+incrementOfLaps()
+console.log(lapsCompleted)
