c766cf3620081beac446ef3ea68c1f9815d008f0
anonymouspython96 <emilianmail96@gmail.com>
2025-10-24 14:29:48 +0200

    writed my first function

--- JavaScript/Write my first script in JavaScript/index.js
+++ JavaScript/Write my first script in JavaScript/index.js
@@ -1,13 +1,22 @@
-let myAge = 29
-let humanDogRatio = 7
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
+// let myAge = 29
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
 
-let bonusPoints = 50
-console.log(bonusPoints)
-bonusPoints += 50
-console.log(bonusPoints)
-bonusPoints -= 75
-console.log(bonusPoints)
-bonusPoints += 45
-console.log(bonusPoints)
+// let bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints += 50
+// console.log(bonusPoints)
+// bonusPoints -= 75
+// console.log(bonusPoints)
+// bonusPoints += 45
+// console.log(bonusPoints)
+
+function increment() {
+    console.log("The button was clicked")
+} // best function ever
+
+function answerOfTheFoundamentalQuestion() {
+    console.log(42)
+}
+answerOfTheFoundamentalQuestion()
